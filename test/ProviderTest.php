66fba7537ef01b70965330045639d6b12d2069b0
Dmitry <dmitry@pereslegin.ru>
2016-06-22 12:16:30 +0300

    Cover bug with tests

---
+++ test/ProviderTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace AutowpTest\OAuth2\Client;
+
+use Autowp\OAuth2\Client\Provider\Vk;
+
+use League\OAuth2\Client\Token\AccessToken;
+
+/**
+ * @group Autowp\OAuth2\Client\Provider\Vk
+ */
+class ProviderTest extends \PHPUnit_Framework_TestCase
+{
+    
+    public function testSetLangPropery()
+    {
+        $provider = new Vk();
+        
+        $provider->setLang('en');
+        
+        $token = new AccessToken([
+            'access_token' => 'test-token'
+        ]);
+        
+        $url = $provider->getResourceOwnerDetailsUrl($token);
+        
+        $this->assertContains('lang=en', $url);
+    }
+    
+    public function testResetLangPropery()
+    {
+        $provider = new Vk();
+    
+        $provider->setLang('en');
+        $provider->setLang(null);
+    
+        $token = new AccessToken([
+            'access_token' => 'test-token'
+        ]);
+    
+        $url = $provider->getResourceOwnerDetailsUrl($token);
+    
+        $this->assertNotContains('lang=en', $url);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetLangProperyThrownsInvalidArgumentExceptionOnInt()
+    {
+        $provider = new Vk();
+    
+        $provider->setLang(1);
+    }
+    
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetLangProperyThrownsInvalidArgumentExceptionOnObject()
+    {
+        $provider = new Vk();
+    
+        $provider->setLang($provider);
+    }
+    
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetLangProperyThrownsInvalidArgumentExceptionOnArray()
+    {
+        $provider = new Vk();
+    
+        $provider->setLang([]);
+    }
+
+}
